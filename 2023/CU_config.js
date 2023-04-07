151c700472d8ef57175af5bf5c4f279a211cef2d
tatees <102478462+tatees@users.noreply.github.com>
2023-04-06 20:48:48 -0400

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023necmp",
+      "defaultValue": "2023necmp2",
       "required": "true",
       "disabled": "true"
     },
